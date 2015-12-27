062071ea1b9e83aa098ca3dd8e2d1ae60198ee55
Thomas Ingram <sleepmore@openmailbox.org>
2015-12-27 00:33:42 -0500

    Added full popup support
    I tweaked some of the colors to my liking in the popup-tip-face which was already previously supported.

--- gruvbox-theme.el
+++ gruvbox-theme.el
@@ -192,8 +192,13 @@
     `(js2-jsdoc-html-tag-name           ((t (:background nil :foreground ,gruvbox-light4 ))))
     `(js2-jsdoc-html-tag-delimiter      ((t (:background nil :foreground ,gruvbox-light3 ))))
 
-    ;; popup-tip
-    `(popup-tip-face                    ((t (:background ,gruvbox-dark1 :foreground ,gruvbox-neutral_yellow))))
+
+    ;; popup
+    `(popup-face                                ((t (:foreground ,gruvbox-light1 :background ,gruvbox-dark1))))
+    `(popup-menu-mouse-face                     ((t (:foreground ,gruvbox-light0 :background ,gruvbox-faded_green))))
+    `(popup-menu-selection-face                 ((t (:foreground ,gruvbox-light0 :background ,gruvbox-faded_green))))
+    `(popup-tip-face                            ((t (:foreground ,gruvbox-light2 :background ,gruvbox-dark2))))
+
 
     ;; helm
     `(helm-M-x-key                              ((t ( :foreground ,gruvbox-neutral_orange  ))))
