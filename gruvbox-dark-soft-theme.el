b802b127e1105c00ac4b7de3ccbfa8683d7e2a35
Martijn Terpstra <bigmartijn@gmail.com>
2018-03-13 17:45:08 +0100

    Added support for which-function-mode

--- gruvbox-dark-soft-theme.el
+++ gruvbox-dark-soft-theme.el
@@ -12,7 +12,7 @@
 ;;              Eduardo Lavaque <me@greduan.com>
 ;;
 ;; URL: http://github.com/greduan/emacs-theme-gruvbox
-;; Version: 1.22.3
+;; Version: 1.23.0
 
 ;; Package-Requires: ((autothemer "0.2"))
 
