7f9f0514ce85b6fa14960207df270cda45b8c3c0
Jasonm23 <jasonm23@gmail.com>
2018-06-24 08:29:54 +0800

    bump 1.24.0

--- gruvbox.el
+++ gruvbox.el
@@ -12,7 +12,7 @@
 ;;              Eduardo Lavaque <me@greduan.com>
 ;;
 ;; URL: http://github.com/greduan/emacs-theme-gruvbox
-;; Version: 1.23.0
+;; Version: 1.24.0
 
 ;; Package-Requires: ((autothemer "0.2"))
 
